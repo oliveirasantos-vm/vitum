3a0501ed2db1ce2430b71471dbea74af22446976
Vitor Mateus de Oliveira dos Santos <oliveirasantos.vm@gmail.com>
2021-01-02 19:03:21 -0300

    update biography component.

--- src/components/Biography.vue
+++ src/components/Biography.vue
@@ -1,25 +1,57 @@
 <template>
-  <div id="biography">
-    <div class="p item-1"></div>
-    <div class="img item-2"></div>
+  <table id="biography">
+    <tr>
+      <td class="p item-1">
+      </td>
+      <td class="img item-2">
+          Olá, meu nome é Vitor Mateus, mas pode me chamar de vitum. Nasci em Gravataí, em 16 de janeiro de 2001, no Rio Grande do Sul, na região metropolitana de Porto Alegre. Foi lá, onde passei boa parde da minha infância. Lá me lembro do primeiro computador que usei, ele era branco, até meio amarelado, a tela parecia uma daquelas televisões antigas, a partir daí, comecei a me apaixonar pela área da computação e informática e hoje sou um desenvolvedor e web designer.
+      </td>
+    </tr>
+    <tr>
+      <td class="p item-3">
 
-    <div class="p item-3"></div>
-    <div class="img item-4"></div>
+      </td>
+      <td class="img item-4">
 
-    <div class="p item-5"></div>
-    <div class="img item-6"></div>
+      </td>
+    </tr>
+    <tr>
+      <td class="p item-5">
 
-    <div class="p item-7"></div>
-    <div class="img item-8"></div>
-  </div>
+      </td>
+      <td class="img item-6">
+
+      </td>
+    </tr>
+    <tr>
+      <td class="p item-7">
+
+      </td>
+      <td class="img item-8">
+
+      </td>
+    </tr>
+  </table>
 </template>
 
 <style scoped>
-#biography{
-    background-image: url('../assets/biography-bg.svg');
-    margin-left: auto;
-    margin-right: auto;
-    width: 768px;
-    height: 1140px;
+#biography {
+  background-image: url("../assets/biography-bg.svg");
+  background-color: #f0f7d4;
+  color: #110934;
+  background-position: center bottom;
+  background-repeat: no-repeat;
+  padding-left: 50px;
+  padding-right: 50px;
+  width: 100%;
+  height: 1140px;
+}
+td{
+    width: 50%;
+    padding-top: 150px ;
+    padding-right: 50px;
+    font-size: 16px;
+    text-align: justify;
+    line-height: 26px;
 }
 </style>
